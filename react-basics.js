c0dea2fb84c386722bfae7d2f6e0eadde641466a
Hesham.Samir <hesham.samir@isoft.ae>
2025-11-17 14:38:21 +0200

    add new files

---
+++ react-basics.js
@@ -0,0 +1,9 @@
+// const button = document.createElement("button");
+// button.innerText = "Click Me";
+// button.onclick = () => alert("Button Clicked!");
+// document.body.appendChild(button);
+
+// const button2 = document.createElement("button");
+// button2.innerText = "No, Click Me!";
+// button2.onclick = () => alert("Button 2 Clicked!");
+// document.body.appendChild(button2);
